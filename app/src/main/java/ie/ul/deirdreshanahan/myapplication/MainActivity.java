5590f7856bc8a604b8e4c194409ec8f19ab10dea
16230256 <39533070+16230256@users.noreply.github.com>
2018-09-22 16:08:38 +0100

    part1 Score Calculator top buttons working and colour working

--- app/src/main/java/ie/ul/deirdreshanahan/myapplication/MainActivity.java
+++ app/src/main/java/ie/ul/deirdreshanahan/myapplication/MainActivity.java
@@ -2,12 +2,79 @@ package ie.ul.deirdreshanahan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mColorIdentificationScore = 0;
+    private int mTotalScore = 0;
+    private TextView mMessageTextView;
+
+    private EditText mNearBallDistanceEditText;
+    private EditText mFarBallDistanceEditText;
+    private EditText mRobotHomeDistanceEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // This method is called when the app launches
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Code you add goes after the setContentView
+        mMessageTextView = findViewById(R.id.message_color_points_initial);
+
+        final Button resetButton = findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mColorIdentificationScore = 0;
+                updateView();
+            }
+        });
+
+
+        // temp area
+        //mMessageTextView.setText("Niall is the coolest programmer ever!!!!!");
+        //Log.d("HB", "This is a log cat log. Logs are important!");
+
+        //  Example crash!!!
+        //mMessageTextView = findViewById(0);
+        //mMessageTextView.setText("This will crash!");
+
+    }
+
+
+    public void handle3fixes(View view) {
+        mColorIdentificationScore = 0;
+        updateView();
+    }
+
+    public void handle2fixes(View view) {
+        mColorIdentificationScore = 25;
+        updateView();
+    }
+
+    public void handle1fixes(View view) {
+        mColorIdentificationScore = 75;
+        updateView();
+    }
+
+    public void handle0fixes(View view) {
+        mColorIdentificationScore = 150;
+        updateView();
+    }
+
+
+
+
+    private void updateView() {
+
+        mMessageTextView.setText(getString(R.string.message_color_points, mColorIdentificationScore));
+        mMessageTextView.setText(getString(R.string.message_total_score, mColorIdentificationScore));
+        mTotalScore = mColorIdentificationScore;
+
     }
 }
